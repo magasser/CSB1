28167ff7043826e129ec879104d8b09204e74666
Beastlike3773 <manuel.gasser.ch@gmail.com>
2018-10-18 23:40:00 +0200

    [CHG] worked on calculator

--- OwnProgramms/calculator/calculator64.asm
+++ OwnProgramms/calculator/calculator64.asm
@@ -6,25 +6,30 @@
 ; Descpription:		A simple calculator in assembly for Linux
 
 SECTION .data
-	FirstN	 db 	"Enter first number:", 0Ah
-;	SecondN	 db 	"Enter second number:", 0Ah
-	FirstL	 equ	$-FirstN
-;	SecondL	 equ	$-SecondN
+	FirstN	 	db 	"Enter first number:", 0Ah
+	FirstL	 	equ	$-FirstN
+	SecondN	 	db 	"Enter second number:", 0Ah
+	SecondL	 	equ	$-SecondN
+	Operand  	db	"Enter your operand:", 0Ah
+	OperandL 	equ	$-Operand
+	LineFeed 	db	0Ah
+	LineFeedL 	equ	$-LineFeed
+	Result	 	db	"Result: ", 0Ah
+	ResultL	 	equ	$-Result
 	
 SECTION .bss
 	first	resb 16
 	second  resb 16
+	operand resb 16
+	result	resb 16
 SECTION .text
 
 global _start
 
 _start:	
 	
-	call _printFirst
-	call _getFirst
-;	call _printSecond
-;	call _getSecond
-
+	jmp	_printFirst
+	
 Exit:
 	mov	RAX, 60
 	mov	RDI, 0
@@ -36,7 +41,7 @@ _printFirst:
 	mov	RSI, FirstN
 	mov	RDX, FirstL
 	syscall
-	ret
+	jmp	_getFirst
 
 _printSecond:
 	mov 	RAX, 1
@@ -44,7 +49,15 @@ _printSecond:
 	mov	RSI, SecondN
 	mov	RDX, SecondL
 	syscall
-	ret
+	jmp	_getSecond
+
+_printOperand:
+	mov	RAX, 1
+	mov	RDI, 1
+	mov	RSI, Operand
+	mov	RDX, OperandL
+	syscall
+	jmp	_getOperand
 
 _getFirst:
 	mov	RAX, 0
@@ -52,7 +65,41 @@ _getFirst:
 	mov	RSI, first
 	mov	RDX, 16
 	syscall
-	ret
+
+
+	mov 	RCX, 1
+	mov	RBX, 0
+	mov	RDX, 0
+Repeat:
+	mov	RAX, [first]
+	and	RAX, 00FFh
+	sub	RAX, 030h
+	mov	RAX, [first]
+	push	RBX
+	mov	RBX, 0100h
+	div	RBX
+	pop	RBX
+	mov	[first], RAX
+	push	RBX
+	mov	RBX, RCX
+Loop:
+	push	RBX
+	mov	RBX, 010h
+	mul	RBX
+	pop	RBX
+	dec	RBX
+	cmp	RBX, 0
+	je	Loop
+	
+	mov	RBX, 010h
+	div	RBX
+	pop	RBX
+	add	RBX, RAX
+	cmp	RCX, 16
+	inc	RCX
+	je	Repeat
+	push	RBX
+	jmp 	_printSecond
 
 _getSecond:
 	mov	RAX, 0
@@ -60,4 +107,59 @@ _getSecond:
 	mov	RSI, second
 	mov	RDX, 16
 	syscall
-	ret
+
+	mov	RAX, [second]
+	and	RAX, 00FFh
+	sub	RAX, 030h
+	push	RAX
+	jmp	_printOperand
+
+_getOperand:
+	mov	RAX, 0
+	mov	RDI, 0
+	mov	RSI, operand
+	mov	RDX, 16
+	syscall
+	jmp	_calculateResult
+
+_calculateResult:
+	mov	RAX, [operand]
+	mov	RBX, 0A2Bh
+	CMP	RAX, RBX
+	JE 	Add
+	CMP 	RAX, 0A2Dh
+	JE 	Sub
+
+Add:
+	pop	RBX
+	pop	RAX, 
+	add	RAX, RBX
+	jmp	_printResult
+Sub:
+	pop	RBX
+	pop	RAX
+	sub	RAX, RBX
+	jmp	_printResult
+	
+_printResult:
+	mov	RBX, RAX
+	add	RBX, 030h
+	mov	[result], RBX
+	mov	RAX, 1
+	mov	RDI, 1
+	mov	RSI, Result
+	mov	RDX, ResultL
+	syscall
+
+	mov	RAX, 1
+	mov	RDI, 1
+	mov	RSI, result
+	mov	RDX, 16
+	syscall
+	
+	mov	RAX, 1
+	mov	RDI, 1
+	mov	RSI, LineFeed
+	mov	RDX, LineFeedL
+	syscall
+	jmp	Exit
